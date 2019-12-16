2d38294a2319b59d5a4ffb743ab89661c08a0f65
Alan Xu <alanxu2013@gmail.com>
2019-12-16 13:16:41 -0500

    Hi

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -10,7 +10,7 @@
 
     class ViewController: UIViewController {
 
-        var index = 0
+        var index:Int? = 0
         var quoteArray = ["Cat 1", "Cat 2", "Cat 3", "Cat 4", "Cat 5"]
         var filenameArray = ["Unknown.jpeg", "Unknown-1.jpeg", "Unknown-2.jpeg", "Unknown-3.jpeg", "Unknown-4.jpeg"]
         var show = [true, true, true, true, true]
@@ -21,7 +21,7 @@
 
         @IBAction func changeImages(_ sender: UIButton) {
             showImage()
-            index+=1
+            index!+=1
         }
         @IBAction func randomImages(_ sender: UIButton) {
             index = Int.random(in: 0...4)
@@ -29,38 +29,55 @@
         }
         
         func showImage() {
-            
-            if index > quoteArray.count - 1{
+            guard show.firstIndex(of: true) != nil
+                else{
+                    return
+            }
+            if index! > quoteArray.count - 1{
                 index = show.firstIndex(of: true)!
                 }
             
-            while show[index] == false{
-                if index > quoteArray.count - 2{
+            while show[index!] == false{
+                if index! > quoteArray.count - 2{
                     index = show.firstIndex(of: true)!
-                    if index == nil {
-                        break
-                    }
                 } else {
-                    index+=1
+                    index!+=1
                 }
             }
             
-            ImageHandler.image = UIImage(named: filenameArray[index])
-            Label.text = quoteArray[index]
+            ImageHandler.image = UIImage(named: filenameArray[index!])
+            Label.text = quoteArray[index!]
 
         }
         
         
         @IBAction func hideImages(_ sender: UIButton) {
-            show[index - 1] = false
+            guard show.firstIndex(of: true) != nil
+                else{
+                    return
+            }
+            
+            show[index! - 1] = false
             showImage();
-            index+=1
+            
+            if show.firstIndex(of: true) == nil {
+                ImageHandler.image = UIImage(named: "")
+                Label.text = ""
+            }
+            index!+=1
         }
         
         @IBAction func showHiddenImages(_ sender: UIButton) {
+            if show.firstIndex(of: true) == nil || index! < 0{
+                index! = 0
+            } else {
+                index!-=1
+            }
             for n in 0...4  {
                 show[n] = true
             }
+            showImage()
+            index!+=1
         }
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -69,28 +86,3 @@
 
 
     }
-    
-    //                switch n {
-    //                    case 0:
-    //                        ImageHandler.image = UIImage(named: filenameArray[0])
-    //                        Label.text = quoteArray[0]
-    //                        index+=1
-    //                    case 1:
-    //                        ImageHandler.image = UIImage(named: filenameArray[1])
-    //                        Label.text = quoteArray[1]
-    //                        index+=1
-    //                    case 2:
-    //                        ImageHandler.image = UIImage(named: filenameArray[2])
-    //                        Label.text = quoteArray[2]
-    //                        index+=1
-    //                    case 3:
-    //                        ImageHandler.image = UIImage(named: filenameArray[3])
-    //                        Label.text = quoteArray[3]
-    //                        index+=1
-    //                    case 4:
-    //                        ImageHandler.image = UIImage(named: filenameArray[4])
-    //                        Label.text = quoteArray[4]
-    //                        index = 0
-    //                    default: break
-    //                }
-
